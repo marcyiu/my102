2f9e87587219e44bdd62962915b515896de5461a
M-Yiu <yiu.marcus@gmail.com>
2022-06-30 16:20:10 +0800

    used navigation rail instead of drawer

--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:my102/main.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(MyApp.appTitle)),
+      // appBar: AppBar(title: Text(MyApp.appTitle)),
       drawer: Drawer(
         child: ListView(
           children: [
@@ -54,6 +61,31 @@ class HomeScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      body: Row(
+        children: [
+          NavigationRail(
+            destinations: const <NavigationRailDestination>[
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite_border),
+                selectedIcon: Icon(Icons.favorite),
+                label: Text('First'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.bookmark_border),
+                selectedIcon: Icon(Icons.book),
+                label: Text('Second'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.star_border),
+                selectedIcon: Icon(Icons.star),
+                label: Text('Third'),
+              ),
+            ],
+            selectedIndex: _selectedIndex,
+            extended: true,
+          ),
+        ],
       ),
     );
   }
